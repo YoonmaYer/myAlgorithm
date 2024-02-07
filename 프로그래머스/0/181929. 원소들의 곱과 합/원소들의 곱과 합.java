c93431de87bff2b3a6cfb0d028e7b6d7d07ea1b8
kang yoon <88176134+YoonmaYer@users.noreply.github.com>
2024-02-07 09:07:11 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.01 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int sum = 0;
+        int mul = 1;
+        
+        for (int i = 0; i < num_list.length; i++) {
+            sum += num_list[i];
+        }
+        
+        int sumsum = sum * sum;
+        
+        for (int i = 0; i < num_list.length; i++) {
+            mul *= num_list[i];
+        }
+        
+        if (mul < sumsum) {
+            answer = 1;
+        } else {
+            answer = 0;
+        }
+        
+        
+        return answer;
+    }
+}
